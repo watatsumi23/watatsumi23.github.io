a16c9391e1bcfae4591d4e19670ae12624df818d
Felipe de Jesus Perez Lopez <78581187+watatsumi23@users.noreply.github.com>
2025-11-07 00:52:26 -0600

    cambio 5

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saral Theme'
+export const SITE_TITLE = 'watatsumi23 blog'
 export const SITE_DESCRIPTION =
 	'A simple theme for personal blog sites, created for Astro framework'
 
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = '/'
 
 export const SocialLinks = [
 	{
