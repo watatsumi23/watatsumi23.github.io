f5b5b656561800fa835b0c3a07ae89869d9801b6
Felipe de Jesus Perez Lopez <78581187+watatsumi23@users.noreply.github.com>
2025-11-07 00:08:08 -0600

    cambio 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://watatsumi23.github.io',
-	base: '/watatsumi23.github.io',
+	base: '/',
 	integrations: [
 		mdx(),
 		sitemap(),
